111245b2bf8acb4370c5a80c82cbb3c6d291e8b3
Gagan Ahlawat <cg.ahlawat2036@gmail.com>
2025-07-16 23:30:49 +0530

    Cleaned up .gitignore

--- backend/config/multer-config.js
+++
@@ -1,7 +0,0 @@
-const multer = require("multer");
-
-const storage = require("../utils/cloudinaryStorage");
-
-const upload = multer({ storage });
-
-module.exports = upload;
--- backend/utils/setAuthCookie.js
+++
@@ -1,8 +0,0 @@
-module.exports = (res, token) => {
-    res.cookie("token", token, {
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000,
-        sameSite: "none",
-        secure: process.env.NODE_ENV === "production",
-    });
-};
